438745408770151db1363c21d86c5c578d0a5a05
jysfc <63255178+jysfc@users.noreply.github.com>
2020-10-16 10:35:59 -0700

    added flat

--- array-methods.js
+++ array-methods.js
@@ -21,26 +21,34 @@
 // console.log(`The last day in this array is ${lastDay}.`);
 
 const holidays = [
-   `Wednesday, January 1 – New Year's Day`,
-   `Monday, January 20 – Birthday of Martin Luther King, Jr.`,
-   `Monday, February 17 – Washington's Birthday`,
-   `Monday, May 25 – Memorial Day`,
-   `Friday, July 4 – Independence Day`,
-   `Monday, September 7 – Labor Day`,
-   `Monday, October 12 – Columbus Day`,
+   `New Year’s Day (January 1)`,
+   `Birthday of Martin Luther King, Jr. (Third Monday in January)`,
+   `Washington’s Birthday (Third Monday in February)`,
+   `Memorial Day (Last Monday in May)`,
+   `Independence Day (July 4)`,
+   `Labor Day (First Monday in September)`,
+   `Columbus Day (Second Monday in October)`,
 ];
-const firstDay = holidays[0];
-console.log(`The first day in this array is ${firstDay}.`);
-const holidaysArrLength = holidays.length; //length is not zero based
-console.log(`The length of this array is ${holidaysArrLength}.`);
-const lastDay = holidays[holidaysArrLength - 1];
-console.log(`The last day in this array is ${lastDay}.`);
 
-const favoriteHoliday = `Friday, July 4 – Independence Day`;
+const newHolidaysFromHr = [
+   //arr of arr
+   [`Veterans Day (November 11)`, 2020, `Megan`],
+   [`Thanksgiving Day (Fourth Thursday in November)`, 2020, `Megan`],
+   [`Christmas Day (December 25)`, 2020, `Megan`],
+];
+
+const firstDay = holidays[0];
+// console.log(`The first day in this array is ${firstDay}.`);
+const holidaysArrLength = holidays.length; //length is not zero based
+// console.log(`The length of this array is ${holidaysArrLength}.`);
+const lastDay = holidays[holidaysArrLength - 1];
+// console.log(`The last day in this array is ${lastDay}.`);
+
+const favoriteHoliday = `Independence Day (July 4)`;
 const indexOfIndependenceDay = holidays.indexOf(favoriteHoliday); //list first index of element given
-console.log(
-   `${favoriteHoliday} is found at the ${indexOfIndependenceDay} of the holidays array.`
-);
+// console.log(
+//    `${favoriteHoliday} is found at the ${indexOfIndependenceDay} of the holidays array.`
+// );
 
 const specificHoliday = `Mike's Birthday (April 21)`;
 
@@ -56,25 +64,31 @@ const specificHoliday = `Mike's Birthday (April 21)`;
 
 // works the same with lastIndexOf() (search backwards, end to start)
 if (holidays.includes(specificHoliday)) {
-   console.log(
-      `Yes, ${specificHoliday} is a holiday. It was found at the ${holidays.indexOf(
-         specificHoliday
-      )} index in the list of holidays.`
-   );
+   // console.log(
+   //    `Yes, ${specificHoliday} is a holiday. It was found at the ${holidays.indexOf(
+   //       specificHoliday
+   //    )} index in the list of holidays.`
+   // );
 } else {
-   console.log(`No, ${specificHoliday} is not a holiday.`);
+   //console.log(`No, ${specificHoliday} is not a holiday.`);
 }
 
 // find element and replace with (need index to change element in array)
-const replaceableHoliday = `Monday, February 17 – Washington's Birthday`;
+const replaceableHoliday = `Washington’s Birthday (Third Monday in February)`;
 const indexOfReplaceableHoliday = holidays.indexOf(replaceableHoliday);
-console.log(
-   `We are going to replace ${replaceableHoliday} at the ${indexOfReplaceableHoliday} index of the holiays list.`
-);
+// console.log(
+//    `We are going to replace ${replaceableHoliday} at the ${indexOfReplaceableHoliday} index of the holiays list.`
+// );
 
 // creating new list (array) by adding ... or Array.from
 // spread ops https://www.samanthaming.com/tidbits/35-es6-way-to-clone-an-array/
 const newHolidays = [...holidays];
 newHolidays[indexOfReplaceableHoliday] = specificHoliday;
-console.log(`Here's a list of our old holidays: `, holidays);
+// console.log(`Here's a list of our old holidays: `, holidays);
 console.log(`Here's a list of our new holidays: `, newHolidays);
+console.log(`Here's a list of our new holidays from HR: `, newHolidaysFromHr);
+
+const allHolidays = newHolidays.concat(newHolidaysFromHr); //combining two array
+console.log(`Here's a list of all holidays: `, allHolidays);
+const allFlatHolidays = allHolidays.flat();
+console.log(`Here's a list of all holidays, but flat: `, allFlatHolidays);
