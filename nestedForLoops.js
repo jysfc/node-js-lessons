505f6104f71a7699f3453b0a2fef6ab2f42ffd98
jysfc <63255178+jysfc@users.noreply.github.com>
2020-10-16 23:34:29 -0700

    finished 127b

--- nestedForLoops.js
+++ nestedForLoops.js
@@ -1,16 +1,26 @@
 const users = [
-   // data from excel
-   [`Mike`, `Zetlow`, `mike@gmail.com`],
-   [`John`, `Smith`, `john@gmail.com`],
-   [`Sue`, `Smith`, `sue@gmail.com`],
+   // data from excel 3 rows, 4 columns
+   [`MIKE`, `ZETLOW`, `MIKE@gmail.com`, `met him at Demo Day`],
+   [`John`, `John@gmail.com`],
+   [20200921, `sue`],
 ];
 
-// Please provide a list of email address for our mailing list
+// Please provide normalized data for each user field (everything lower cased)
+// Please keep in their original rows
 
-const emails = [];
+let normalizedUsers = [];
 
 for (let i = 0; i < users.length; i++) {
-   console.log(users[i]); // user[0], user[1], user[2]
+   // run data 3 times
+   const userFields = users[i];
+   let newUserFields = [];
+   // console.log(users); // user[0], user[1], user[2] #=column
+   for (let i2 = 0; i2 < userFields.length; i2++) {
+      const userField = String(userFields[i2]); //adding string() converts everything to string
+      const lowerCasedField = userField.toLowerCase();
+      newUserFields = newUserFields.concat(lowerCasedField);
+   }
+   normalizedUsers = normalizedUsers.concat([newUserFields]);
 }
 
-console.log(users[2]);
+console.log(normalizedUsers);
