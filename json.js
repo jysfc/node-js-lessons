db6a067e51ac2165620c80f1e7b1155d82c846c5
jysfc <63255178+jysfc@users.noreply.github.com>
2020-10-28 14:41:33 -0700

    added json.json finished 136A

--- json.js
+++ json.js
@@ -4,9 +4,35 @@ const user = {
    password: getPassword(),
    createdAt: getCreatedAt(),
    id: getId(),
+   // list of social profiles
+   socialProfiles: [
+      {
+         site: `facebook`,
+         username: `mikez`,
+         id: `00600756456`,
+         profilePic: ``,
+         image: {
+            small: `small.jpg`,
+            medium: `medium.jpg`,
+            large: `large.jpg`, // large2.jpg
+         },
+      },
+      {
+         site: `twitter`,
+         username: `mikezetlow`,
+         id: `a4b892cd3f`,
+         thumbnail: ``,
+      },
+   ],
 };
 
-console.log(user); // adding .label to target just the one label
+const indexOfFacebook = 0;
+// console.log(user.socialProfiles[indexOfFacebook].image.small); // small.jpg
+const userAsJson = JSON.stringify(user); // value to JSON str
+const userAsObj = JSON.parse(userAsJson); //understand JSON in a better format
+console.log(userAsObj);
+
+// Math.round(), Object.assign({}) = global objects
 
 /* functions start here */
 
@@ -16,7 +42,7 @@ function getEmail() {
 }
 function getPassword() {
    // get the users password from the input with jQuery
-   return `skywalker83`;
+   return `skywalker84`;
 }
 function getCreatedAt() {
    // generate createdAt number, e.g. 1600730742258
