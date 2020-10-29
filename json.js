8ab688ee2f650c885910ea695867527c00578bed
jysfc <63255178+jysfc@users.noreply.github.com>
2020-10-28 19:56:12 -0700

    finished 137A deep copying

--- json.js
+++ json.js
@@ -30,12 +30,40 @@ const indexOfFacebook = 0;
 // console.log(user.socialProfiles[indexOfFacebook].image.small); // small.jpg
 const userAsJson = JSON.stringify(user); // value to JSON str
 const userAsObj = JSON.parse(userAsJson); //understand JSON in a better format
-console.log(userAsObj);
+// console.log(userAsObj);
 
 // Math.round(), Object.assign({}) = global objects
+// shallow copy only goes 1 level deep
+// undefined returns false
+
+newUser = deepCopy(undefined); // deep copy, losing all previous references
+console.log(newUser);
+if (newUser !== undefined) {
+   // if new user is not undefined, consolelog large3
+   newUser.socialProfiles[indexOfFacebook].image.large = "large3.jpg";
+   console.log("-----------------");
+   console.log(newUser.socialProfiles);
+}
+// otherwise show the social profiles
+console.log("-----------------");
+console.log(user.socialProfiles);
 
 /* functions start here */
 
+function deepCopy(obj) {
+   const str = JSON.stringify(obj);
+   return safelyParseJson(str);
+}
+// to prevent crashing
+function safelyParseJson(str) {
+   try {
+      JSON.parse(str);
+   } catch {
+      // if error return the original value
+      return str;
+   }
+   return JSON.parse(str);
+}
 function getEmail() {
    // get the users email from the input with jQuery
    return `mike@gmail.com`;
