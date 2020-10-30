387945cfac1cf94a530abcd6d1aaf30ce3a133ab
jysfc <63255178+jysfc@users.noreply.github.com>
2020-10-30 16:49:59 -0700

    added filter

---
+++ filter.js
@@ -0,0 +1,21 @@
+const users = [
+   { name: "Mike", isActive: true, createdAt: 1601234512420 },
+   { name: "John", isActive: true, createdAt: 1601234512430 },
+   { name: "Michelle", isActive: false, createdAt: 1601234512450 },
+];
+
+//filter is like boolean, true & false, true will be added to the new list, false will not
+const mostRecentUsers = users.filter((user) => {
+   return user.createdAt >= 1601234512430;
+});
+
+console.log(mostRecentUsers);
+
+// another way to write it;
+// const mostRecentUsers = users.filter((user) => {
+//    if (user.createdAt >= 1601234512430) {
+//       return true;
+//    } else {
+//       return false;
+//    }
+// });
