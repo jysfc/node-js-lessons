5b31fdbd877992c6ccf0854e26a24469caf85cc0
jysfc <63255178+jysfc@users.noreply.github.com>
2020-11-01 23:23:39 -0800

    adding chaining 141a

---
+++ chaining.js
@@ -0,0 +1,81 @@
+const users = [
+   {
+      name: "Mike",
+      isActive: true,
+      createdAt: 1601234512420,
+      socialProfiles: [
+         {
+            site: "twitter",
+            username: "mzetlow",
+         },
+         {
+            site: "facebook",
+            username: "fbmikezetlow",
+            profilePhoto: {
+               sm: "small.jpg",
+               lg: "large.jpg",
+            },
+         },
+      ],
+   },
+   {
+      name: "John",
+      isActive: true,
+      createdAt: 1601234512430,
+      socialProfiles: [
+         {
+            site: "facebook",
+            username: "fbjsmith",
+            profilePhoto: {
+               sm: "small.jpg",
+               lg: "large.jpg",
+            },
+         },
+      ],
+   },
+   {
+      name: "Michelle",
+      isActive: false,
+      createdAt: 1601234512450,
+      socialProfiles: [
+         {
+            site: "twitter",
+            username: "michelledoe",
+         },
+      ],
+   },
+];
+
+const fbUsers = users.map((user) => {
+   // name + fb name only
+   const newUser = {
+      name: user.name,
+      fbName: getFbName(user),
+   };
+   return newUser;
+});
+
+function getFbName(user) {
+   // return FB name if they have one
+   // if they don't return empty string: ""
+   let fbName = ``;
+   user.socialProfiles.forEach((profile) => {
+      if (profile.site === "facebook") {
+         fbName = profile.username;
+      }
+   });
+   return fbName;
+}
+
+function getFbNameUsingFP(user) {
+   const fbProfiles = user.socialProfiles.filter((profile) => {
+      return profile.site === "facebook";
+   });
+
+   if (fbProfiles.length > 0) {
+      return fbProfiles[0].username;
+   }
+   return "";
+}
+
+console.log(fbUsers);
